aad6418b7251b590299062308772f3644bc7d046
crotert <crotert@gmail.com>
2019-04-05 22:09:40 -0500

    Initial Check-in of Project Skeleton

---
+++ project/src/test/java/edu/depaul/cdm/se452/project/ProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.depaul.cdm.se452.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
